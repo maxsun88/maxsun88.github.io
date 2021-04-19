a91592f45bd041ba4f769345e41f08440ae3a4b6
joeydb94 <joeybyrne94@gmail.com>
2021-04-18 21:29:53 -0400

    Update cartFunctions.js
    small syntax error fix

--- cartFunctions.js
+++ cartFunctions.js
@@ -26,7 +26,7 @@ function addItemsToCart(){
 		return;
 	}
 	var cartRow = document.createElement('div');
-	var carItems = document.getElementByClassName('cart-items')[0];
+	var cartItems = document.getElementByClassName('cart-items')[0];
 	for (var i = 0; i < productList.length; i++){
 		var cartRowContent = ` <div class="row mb-4">
 								<div class="col-md-5 col-lg-3 col-xl-3">
